0f9e6cb67b5b1604d120d84ee263a0660203df61
John Karahalis <john.karahalis@gmail.com>
2016-11-04 15:35:05 -0400

    Basic listing of all sites

---
+++ README.md
@@ -0,0 +1,4 @@
+## Sample data
+
+[This Mockaroo](https://www.mockaroo.com/54a7ec90) was used to generate sample
+data.
---
+++ gulpfile.babel.js
@@ -0,0 +1,21 @@
+import gulp from 'gulp';
+
+import fs from 'fs';
+
+import data from 'gulp-data';
+import nunjucks from 'gulp-nunjucks';
+
+gulp.task('default', ['build']);
+
+gulp.task('build', ['build:html']);
+
+gulp.task('build:html', () => {
+    const context = {
+        sites: JSON.parse(fs.readFileSync('./sites.json')),
+    }
+
+    gulp.src('templates/index.html')
+        .pipe(data(() => context))
+        .pipe(nunjucks.compile())
+        .pipe(gulp.dest('dist'));
+});
